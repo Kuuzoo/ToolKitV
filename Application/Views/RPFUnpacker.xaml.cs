ea98990647f3b6a92c5113f908ac5f4783506b05
Kuuzoo <nilscosmo@hotmail.com>
2025-02-03 01:30:09 +0100

    Start with RPF Unpacker

---
+++ Application/Views/RPFUnpacker.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace ToolKitV.Views
+{
+    public partial class RPFUnpacker : UserControl
+    {
+        public RPFUnpacker()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
